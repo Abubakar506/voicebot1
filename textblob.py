d6369359d329222365bdbb9610f23060b3788be2
Abubakar506 <91673839+Abubakar506@users.noreply.github.com>
2024-11-05 22:47:36 +0500

    Create textblob.py

---
+++ textblob.py
@@ -0,0 +1,13 @@
+def download_corpora():
+    try:
+        # Check if corpora are already downloaded
+        nltk.data.find('corpora/brown.zip')
+        nltk.data.find('tokenizers/punkt')
+        nltk.data.find('corpora/wordnet.zip')
+        nltk.data.find('taggers/averaged_perceptron_tagger.zip')
+    except LookupError:
+        # If not found, download the required corpora
+        nltk.download('brown', quiet=True)
+        nltk.download('punkt', quiet=True)
+        nltk.download('wordnet', quiet=True)
+        nltk.download('averaged_perceptron_tagger', quiet=True)
